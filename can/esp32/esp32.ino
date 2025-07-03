399f068db0e1f965974e27ba739d5aa85cb643b3
dohieu070404 <dohieu07042004@gmail.com>
2025-07-03 15:43:33 +0700

    cap nhat esp

--- can/esp32/esp32.ino
+++ can/esp32/esp32.ino
@@ -5,14 +5,11 @@
 const char *ssid = "SONGNAM-STAFF";
 const char *password = "songnam@123";
 
+// Server
 const char *serverURL = "http://192.168.1.53:3000/api/user-data";
 
-// ==== Dữ liệu mẫu ====
-String createData = "create,112233,Nguyen Van A,22,170,60,20.8";
-String checkData = "check,112233,_,_,_,_,_";
-String getData = "get,112233,_,_,_,_,_";
-String updateData = "update,112233,Nguyen Van B,23,172,62,21.0";
-String deleteData = "delete,112233,_,_,_,_,_";
+// Dữ liệu
+String createData = "create,121212,Nguyen Van B,25,1.70,68.5,23.7";
 
 void setup()
 {
@@ -24,23 +21,15 @@ void setup()
     Serial.print(".");
     delay(500);
   }
+
   Serial.println("\nWiFi connected");
   Serial.println("IP address: " + WiFi.localIP().toString());
 
-  sendCommandToServer(create);
   delay(1000);
-  sendCommandToServer(checkD);
-  delay(1000);
-  sendCommandToServer(get);
-  delay(1000);
-  sendCommandToServer(update);
-  delay(1000);
-  sendCommandToServer(delete);
+  sendCommandToServer(createData);
 }
 
-void loop()
-{
-}
+void loop() {}
 
 void sendCommandToServer(String csvData)
 {
@@ -48,13 +37,16 @@ void sendCommandToServer(String csvData)
   {
     HTTPClient http;
     http.begin(serverURL);
+    http.setTimeout(10000); // tăng thời gian chờ
     http.addHeader("Content-Type", "application/json");
 
     String jsonPayload = "{\"data\":\"" + csvData + "\"}";
+
+    Serial.println("Payload JSON: " + jsonPayload); // debug
+
     int httpCode = http.POST(jsonPayload);
     String response = http.getString();
 
-    Serial.println("=== Gửi: " + csvData);
     Serial.println("HTTP Code: " + String(httpCode));
     Serial.println("Phản hồi: " + response);
     Serial.println("===================================");
