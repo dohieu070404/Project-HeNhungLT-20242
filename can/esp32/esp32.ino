73255602d13c42183934c329888a899557acaf5e
dohieu070404 <dohieu07042004@gmail.com>
2025-07-03 15:30:40 +0700

    cap nhat esp

--- can/esp32/esp32.ino
+++ can/esp32/esp32.ino
@@ -1,85 +1,68 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
 
-// === WiFi credentials ===
-const char* ssid = "Esp111";
-const char* password = "11223344";
+// WiFi
+const char *ssid = "SONGNAM-STAFF";
+const char *password = "songnam@123";
 
-// === Server API URL ===
-//  <IP-SERVER> bằng IP  mạng 
-const char* serverPostURL = "http://<IP-SERVER>:3000/api/user-data";
-const char* serverGetBaseURL = "http://<IP-SERVER>:3000/api/user-data/";
+const char *serverURL = "http://192.168.1.53:3000/api/user-data";
 
-// === Dữ liệu mẫu để gửi ===
-String csvData = "123456,Nguyen Van A,22,170,60,20.8";  
-String rfidToCheck = "123456";     //checkid                     
+// ==== Dữ liệu mẫu ====
+String createData = "create,112233,Nguyen Van A,22,170,60,20.8";
+String checkData = "check,112233,_,_,_,_,_";
+String getData = "get,112233,_,_,_,_,_";
+String updateData = "update,112233,Nguyen Van B,23,172,62,21.0";
+String deleteData = "delete,112233,_,_,_,_,_";
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-
-  // === Kết nối WiFi ===
   WiFi.begin(ssid, password);
-  Serial.print("conecting WiFi....");
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
-  Serial.println("\n WiFi conected ");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nWiFi connected");
+  Serial.println("IP address: " + WiFi.localIP().toString());
 
-  // === Gửi dữ liệu CSV lên server ===
-  sendCSVData(csvData);
-
-  delay(2000); 
-
-  //Truy vấn theo RFID 
-  getUserByRFID(rfidToCheck);
+  sendCommandToServer(createData);
+  delay(1000);
+  sendCommandToServer(checkData);
+  delay(1000);
+  sendCommandToServer(getData);
+  delay(1000);
+  sendCommandToServer(updateData);
+  delay(1000);
+  sendCommandToServer(deleteData);
 }
 
-void loop() {
-  // Có thể chờ xử lý SPI sau này
+void loop()
+{
 }
 
-//  post string to sever
-void sendCSVData(String csv) {
-  if (WiFi.status() == WL_CONNECTED) {
+void sendCommandToServer(String csvData)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
     HTTPClient http;
-    http.begin(serverPostURL);
+    http.begin(serverURL);
     http.addHeader("Content-Type", "application/json");
 
-    // create body JSON: { "data": "csv string" }
-    String jsonBody = "{\"data\":\"" + csv + "\"}";
-
-    int httpResponseCode = http.POST(jsonBody);
+    String jsonPayload = "{\"data\":\"" + csvData + "\"}";
+    int httpCode = http.POST(jsonPayload);
     String response = http.getString();
 
-    Serial.println(" POST Status: " + String(httpResponseCode));
-    Serial.println(" Response: " + response);
+    Serial.println("=== Gửi: " + csvData);
+    Serial.println("HTTP Code: " + String(httpCode));
+    Serial.println("Phản hồi: " + response);
+    Serial.println("===================================");
 
     http.end();
-  } else {
-    Serial.println(" WiFi chưa kết nối");
   }
-}
-
-// truy vấn  theo RFID từ server
-void getUserByRFID(String rfid_id) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    String fullURL = serverGetBaseURL + rfid_id;
-    http.begin(fullURL);
-
-    int httpResponseCode = http.GET();
-    String payload = http.getString();
-
-    Serial.println("GET Status: " + String(httpResponseCode));
-    Serial.println("Thông tin người dùng:");
-    Serial.println(payload);
-
-    http.end();
-  } else {
+  else
+  {
     Serial.println("WiFi chưa kết nối");
   }
 }
